e53e27e5a954f5b2240a93f78a6d9e338357055d
Anton <toh1napocht@yandex.ru>
2016-12-27 12:54:39 +0200

    fix routing

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -9,9 +9,9 @@ import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: ':username', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'logout', component: LogoutComponent }
+    { path: 'logout', component: LogoutComponent },
+    { path: ':username', component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 export const Routing =  RouterModule.forRoot(routes);
